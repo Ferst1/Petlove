33480fe74d848a6cfbcb65b91b712a7ddfd08a1a
Ferst1 <36508011+Ferst1@users.noreply.github.com>
2024-07-24 16:19:17 +0200

    paketg updated

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import svgr from 'vite-plugin-svgr';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ command }) => ({
-  plugins: [react(), svgr()],
-  resolve: {
-    alias: {
-      '@': '/src',
-      'components': '/src/components',
-    },
-  },
-  base: command === 'serve' ? '/' : '/Petlove/',
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: './index.html',
-    },
-  },
-  publicDir: 'public',
-}));
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: "/PetsLove/",
+  plugins: [react()],
+})
